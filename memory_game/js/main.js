f52c6d75ff5e90a95aad5bb47d6b0c8e4834991c
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2019-09-21 10:41:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,19 @@
-console.log("Up and running!");
+// Game logic for memory game
+const cards = ['queen','queen','king','king'];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
 
-console .log("User Flipped " + cardOne);
-console .log("User Flipped " + cardTwo);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    }
+      else { 
+      	alert("Sorry, Try Again.");
+    }	
+}
