6814b25705963f35b27afcff7c203a9415440eec
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2019-09-22 16:29:30 -0400

    created game logic 3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,10 +4,12 @@ const cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
 
 function checkForMatch() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
 } else {
   console.log("Sorry, try again.");
+}
 }
 }
 
