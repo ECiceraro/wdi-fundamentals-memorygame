d1a1fcb837bb8f87a15a9a1873993d8dccb73ce5
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2019-09-23 15:57:37 -0400

    almost working game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,6 +25,7 @@ const cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
@@ -34,7 +35,9 @@ function checkForMatch() {
 }
 }
 
-function flipCard(cardId) {
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 
@@ -44,5 +47,21 @@ function flipCard(cardId) {
 
 }
 
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
+
+
+
+
